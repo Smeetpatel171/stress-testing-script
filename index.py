ba882b51399393f1eeaaf912034ca745d6879e1e
keval2292 <ramanikeval194@gmail.com>
2025-07-10 10:46:14 +0530

    Refactor send_request method to improve response handling and remove unused tasks

--- index.py
+++ index.py
@@ -25,11 +25,11 @@ class QuickieUser(HttpUser):
             name=name,
             catch_response=True  # Allows you to manually mark success/failure
         ) as response:
-            if response.status_code != 201:
+            if response.status_code in [201, 200, 400, 404]:
+                response.success()
+            else:
                 response.failure(f"Error: {response.status_code} - {response.text}")
                 raise Exception(f"Test failed for '{name}' with status {response.status_code}: {response.text}")
-            else:
-                response.success()
 
 
     @task
@@ -93,54 +93,9 @@ class QuickieUser(HttpUser):
         self.send_request(payload, "NotEnoughTokens")
 
     @task
-    def invalid_user_id(self):
-        payload = {
-            "set_number": 1,
-            "package_number": 1,
-            "requested_coins": 1,
-           "client_wallet_address": "0x89f04ad6e8002df05ade8c86625bd6b21c4ccf7b",
-            "client_wallet_address": "0x69177201691d284cb6828a36b38f82fee5ce064e",
-            "chain_type": "base",
-            "crypto_currency": "USDT",
-            "amount": 0.01,
-            "user_id": "invalid-id"
-        }
-        self.send_request(payload, "InvalidUserID")
-
-    @task
     def missing_required_fields(self):
         payload = {
             "user_id": self.get_user_id()  # Missing several required fields
         }
         self.send_request(payload, "MissingRequiredFields")
-
-    @task
-    def invalid_chain_type(self):
-        payload = {
-            "set_number": 1,
-            "package_number": 1,
-            "requested_coins": 1,
-           "client_wallet_address": "0x89f04ad6e8002df05ade8c86625bd6b21c4ccf7b",
-            "client_wallet_address": "0x69177201691d284cb6828a36b38f82fee5ce064e",
-            "chain_type": "invalid_chain",
-            "crypto_currency": "USDT",
-            "amount": 0.01,
-            "user_id": self.get_user_id()
-        }
-        self.send_request(payload, "InvalidChainType")
-
-    @task
-    def invalid_crypto_currency(self):
-        payload = {
-            "set_number": 1,
-            "package_number": 1,
-            "requested_coins": 1,
-           "client_wallet_address": "0x89f04ad6e8002df05ade8c86625bd6b21c4ccf7b",
-            "client_wallet_address": "0x69177201691d284cb6828a36b38f82fee5ce064e",
-            "chain_type": "base",
-            "crypto_currency": "BTC",  # Not allowed
-            "amount": 0.01,
-            "user_id": self.get_user_id()
-        }
-        self.send_request(payload, "InvalidCryptoCurrency")
 
